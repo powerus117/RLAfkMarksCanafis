e21e8a44083c2731acab6c30d15f9f27c1c5d709
Frans Irgolitsch <f.irgolitsch@outlook.com>
2025-12-06 20:19:41 -0500

    Reformatted file, optimised imports

--- src/main/java/com/marksofgracecooldown/MarksOfGraceCDConfig.java
+++ src/main/java/com/marksofgracecooldown/MarksOfGraceCDConfig.java
@@ -1,24 +1,18 @@
 package com.marksofgracecooldown;
 
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
-import net.runelite.client.config.Units;
-import net.runelite.client.config.Notification;
+import net.runelite.client.config.*;
 
 @ConfigGroup("AfkMarksCanafis")  // Old name from when it was canifis only
-public interface MarksOfGraceCDConfig extends Config
-{
-	@ConfigItem(
-		keyName = "cooldownNotifier",
-		name = "Cooldown notifier",
-		description = "Notify when the cooldown has expired.",
-		position = 0
-	)
-	default Notification notifyMarksOfGraceCD()
-	{
-		return Notification.ON;
-	}
+public interface MarksOfGraceCDConfig extends Config {
+    @ConfigItem(
+            keyName = "cooldownNotifier",
+            name = "Cooldown notifier",
+            description = "Notify when the cooldown has expired.",
+            position = 0
+    )
+    default Notification notifyMarksOfGraceCD() {
+        return Notification.ON;
+    }
 
     @ConfigItem(
             keyName = "swapLeftClickOnWait",
@@ -26,22 +20,20 @@ public interface MarksOfGraceCDConfig extends Config
             description = "Swaps left click of last obstacle while wait is on to prevent accidental lap completion",
             position = 1
     )
-    default boolean swapLeftClickOnWait()
-    {
+    default boolean swapLeftClickOnWait() {
         return false;
     }
 
-	@ConfigItem(
-		keyName = "swapLeftClickTimeLeft",
-		name = "Swap left click on time left",
-		description = "Only swap left click when the cooldown time remaining is below this number.",
-		position = 2
-	)
-	@Units(Units.SECONDS)
-	default int swapLeftClickTimeLeft()
-	{
-		return 180;
-	}
+    @ConfigItem(
+            keyName = "swapLeftClickTimeLeft",
+            name = "Swap left click on time left",
+            description = "Only swap left click when the cooldown time remaining is below this number.",
+            position = 2
+    )
+    @Units(Units.SECONDS)
+    default int swapLeftClickTimeLeft() {
+        return 180;
+    }
 
     @ConfigItem(
             keyName = "leewaySeconds",
@@ -50,30 +42,27 @@ public interface MarksOfGraceCDConfig extends Config
             position = 3
     )
     @Units(Units.SECONDS)
-    default int leewaySeconds()
-    {
+    default int leewaySeconds() {
         return 1;
     }
 
-	@ConfigItem(
-		keyName = "useShortArdougneTimer",
-		name = "Use short Ardougne timer",
-		description = "When having the elite Ardougne diary, there is a 50% chance to reduce the Ardougne cooldown to 2 min. Would you want to be notified after the reduced time or normal time?",
-		position = 4
-	)
-	default boolean useShortArdougneTimer()
-	{
-		return true;
-	}
+    @ConfigItem(
+            keyName = "useShortArdougneTimer",
+            name = "Use short Ardougne timer",
+            description = "When having the elite Ardougne diary, there is a 50% chance to reduce the Ardougne cooldown to 2 min. Would you want to be notified after the reduced time or normal time?",
+            position = 4
+    )
+    default boolean useShortArdougneTimer() {
+        return true;
+    }
 
-	@ConfigItem(
-		keyName = "showDebugValues",
-		name = "Show debug values",
-		description = "Displays plugin debug values like ntp offset and state",
-		position = 5
-	)
-	default boolean showDebugValues()
-	{
-		return false;
-	}
+    @ConfigItem(
+            keyName = "showDebugValues",
+            name = "Show debug values",
+            description = "Displays plugin debug values like ntp offset and state",
+            position = 5
+    )
+    default boolean showDebugValues() {
+        return false;
+    }
 }
